362345a1c6a392982442408c69594abe473b3cf7
Keith Crabtree <keithcrabtree@live.com>
2021-01-14 22:24:48 -0500

    simple game loop implemented

--- Assets/Scripts/Hacker.cs
+++ Assets/Scripts/Hacker.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game configuration data
+    string[] level1Passwords = { "books", "aisle", "shelf", "password", "font", "borrow" };
+    string[] level2Passwords = { "homocide", "locker", "detective", "window", "badge", "fingerprint"};
+
     int currentLevel;
+    string password;
     enum Screen { MainMenu, PasswordEntry, Win};
-    Screen state;
+    Screen state = Screen.MainMenu;
+
+    string[] passwords = new string[] { "book", "shotgun", "satellite" };
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +30,37 @@ public class Hacker : MonoBehaviour
         } else if (state == Screen.MainMenu)
         {
             HandleMenuSelection(input);
+        } else if (state == Screen.PasswordEntry)
+        {
+            CheckPassword(input);
+        } else if (state == Screen.Win)
+        {
+            Terminal.WriteLine("Go again?");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("current state: " + state);
+
+    }
+
+    private void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinMessage();
+        }
+        else
+        {
+            Terminal.WriteLine("Incorrect password, please try again:");
+        }
+    }
+
+    private void DisplayWinMessage()
+    {
+        state = Screen.Win;
+        Terminal.WriteLine("Access granted. Congratulations!");
     }
 
     private void HandleMenuSelection(string input)
@@ -36,11 +68,13 @@ public class Hacker : MonoBehaviour
         if (input == "1")
         {
             currentLevel = 1;
+            password = level1Passwords[2]; // todo: make random later
             StartGame();
         }
         else if (input == "2")
         {
             currentLevel = 2;
+            password = level2Passwords[4];
             StartGame();
         }
         else if (input == "3")
@@ -58,12 +92,11 @@ public class Hacker : MonoBehaviour
         }
     }
 
-
     private void StartGame()
     {
         state = Screen.PasswordEntry;
         Terminal.WriteLine("You have selected level " + currentLevel);
-        Terminal.WriteLine("Please enter your password: ");
+        Terminal.WriteLine("Please enter password: ");
     }
 
     private void DisplayMainMenu()
